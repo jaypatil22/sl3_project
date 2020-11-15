9ebfef68c2c522451e69f1a79b6f1af418c0e298
Vijay Patil <patilvjay22@gmail.com>
2020-11-15 17:11:57 +0530

    layout of all pages done

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
 $(document).ready(function () {
     $('.navbar-nav li a').click(function(e) {
 
+        
         $('.navbar-nav li.active').removeClass('active');
-
         var $parent = $(this).parent();
         // console.log($parent);
         $parent.addClass('active');
         e.preventDefault();
+        var login = $('.active a').text();
+        // console.log(login);
+        // console.log($('.dropdown').text());
+        if(login.indexOf("About Us") == -1) {
+            document.getElementById('logintitle').innerHTML = login;
+            var image = "url('../images/" + login.toLowerCase().split(' ')[0] + ".jpg'";
+            document.getElementById('bg').style.backgroundImage = image;
+        }
     });
 });
 
+
 function submitForm() {
-    var active = $('.navbar-nav li.active a');
+    var active = $('#logintitle');
     var url = "./" + active.text().toLowerCase().split(" ")[0] + ".html";
     console.log(url);
     window.open(encodeURI(url), "_self");
